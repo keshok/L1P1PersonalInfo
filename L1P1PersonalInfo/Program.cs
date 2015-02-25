a088c5fa7dfe4a7e7a149e458792d27ee874eea9
keshok <keshok@live.ca>
2015-02-25 12:51:12 -0500

    Final Commit

--- L1P1PersonalInfo/Program.cs
+++ L1P1PersonalInfo/Program.cs
@@ -10,6 +10,27 @@ namespace L1P1PersonalInfo
     {
         static void Main(string[] args)
         {
+            string firstName;
+            string lastName;
+            string streetAddress;
+            string city;
+            string province;
+            int age;
+
+            firstName = Convert.ToString("Keshok");
+            lastName = Convert.ToString("Sivalingam");
+            streetAddress = Convert.ToString("1950 Kennedy Road");
+            city = Convert.ToString("Scarborough");
+            province = Convert.ToString("Ontario");
+            age = 21;
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Address: {0}, {1}, {2}", streetAddress, city, province);
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("Press any key to continue or exit...");
+            Console.ReadKey();
         }
     }
 }
